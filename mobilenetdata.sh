85110ed124121ce172b26ac481066a348cc5abc2
通晓宇宙 <2581011320@qq.com>
2023-07-13 10:11:53 +0800

    aaa

--- mobilenetdata.sh
+++ mobilenetdata.sh
@@ -4,10 +4,10 @@
 wget https://ascend-professional-construction-dataset.obs.myhuaweicloud.com/deep-learning/flower_photos_train.zip
 
 # 解压训练数据集
-unzip floor_photos_train.zip
+unzip flower_photos_train.zip
 
 # 下载测试数据集
 wget https://ascend-professional-construction-dataset.obs.myhuaweicloud.com/deep-learning/flower_photos_test.zip
 
 # 解压测试数据集
-unzip floor_photos_test.zip
+unzip flower_photos_train.zip
