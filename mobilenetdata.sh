93005636e5542b9b268c555d4b9b6b6e7f489fbd
通晓宇宙 <2581011320@qq.com>
2023-07-13 14:44:40 +0800

    update

--- mobilenetdata.sh
+++ mobilenetdata.sh
@@ -10,7 +10,7 @@ unzip flower_photos_train.zip
 wget https://ascend-professional-construction-dataset.obs.myhuaweicloud.com/deep-learning/flower_photos_test.zip
 
 # 解压测试数据集
-unzip flower_photos_train.zip
+unzip flower_photos_test.zip
 
 # 预训练参数
 wget https://download.mindspore.cn/models/r1.7/mobilenetv2_ascend_v170_imagenet2012_official_cv_top1acc71.88.ckpt
