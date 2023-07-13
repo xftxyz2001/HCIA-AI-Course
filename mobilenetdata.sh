220a420f3902a9b4d6a4142aecefe31461960fde
通晓宇宙 <2581011320@qq.com>
2023-07-13 10:21:10 +0800

    a

--- mobilenetdata.sh
+++ mobilenetdata.sh
@@ -11,3 +11,6 @@ wget https://ascend-professional-construction-dataset.obs.myhuaweicloud.com/deep
 
 # 解压测试数据集
 unzip flower_photos_train.zip
+
+# 预训练参数
+wget https://download.mindspore.cn/models/r1.7/mobilenetv2_ascend_v170_imagenet2012_official_cv_top1acc71.88.ckpt
